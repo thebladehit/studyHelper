4af4f258b2c1d0fa2b65f6921fe738e9a71df077
TheBladeHit <boqdanyarmolka@gmail.com>
2023-08-11 12:18:47 +0300

    Add dev settup to index.js.

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const express = require('express');
 const exhbs = require('express-handlebars');
 const path = require('node:path');
 const mongoose = require('mongoose');
-const { PORT, URL } = require('./config/config');
+const { PORT, URL, EMAIL, NAME } = require('./config/config');
+const User = require('./models/user');
 
 const homeRoutes = require('./routes/home');
 const addRoutes = require('./routes/add');
@@ -29,6 +30,14 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('64d5fc8f9dc7c357c28dc748');
+    req.user = user;
+  } catch (err) {
+    console.log(err);
+  }
+});
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use('/', homeRoutes);
@@ -40,6 +49,15 @@ app.use('/list', listRoutes);
 async function start() {
   try {
     await mongoose.connect(URL, { useNewUrlParser: true });
+    const candidate = await User.findOne();
+    if (!candidate) {
+      const user = new User({
+        email: EMAIL,
+        name: NAME,
+        list: {items: []}
+      });
+      await user.save();
+    }
     app.listen(PORT, () => {
       console.log(`Working at ${PORT}`);
     });
