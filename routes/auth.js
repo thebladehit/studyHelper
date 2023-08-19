37a5f5f40d8442baa0348eefab7c1291a2989a33
TheBladeHit <boqdanyarmolka@gmail.com>
2023-08-19 11:05:43 +0300

    Implement logout route.

--- routes/auth.js
+++ routes/auth.js
@@ -10,6 +10,12 @@ router.get('/login', (req, res) => {
   });
 });
 
+router.get('/logout', (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/auth/login');
+  });
+});
+
 router.post('/login', (req, res) => {
   req.session.isAuthenticated = true;
   res.redirect('/subjects');
