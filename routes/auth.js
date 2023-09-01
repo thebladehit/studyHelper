fb7dab03129d99e1cb9c0722e0bf7ff468c84d36
TheBladeHit <boqdanyarmolka@gmail.com>
2023-09-01 23:26:29 +0300

    Add login validation.

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ const crypto = require('node:crypto');
 const nodemailer = require('nodemailer');
 const { validationResult } = require('express-validator');
 const { EMAIL, PASS } = require('../config/config');
-const { registerValidators } = require('../utils/validations');
+const { registerValidators, loginValidators } = require('../utils/validations');
 const regEmail = require('../emails/registration');
 const resetEmail = require('../emails/reset');
 
@@ -36,28 +36,22 @@ router.get('/logout', (req, res) => {
   });
 });
 
-router.post('/login', async (req, res) => {
+router.post('/login', loginValidators, async (req, res) => {
   try {
-    const { email, password } = req.body;
-    const candidate = await User.findOne({ email });
-    if (candidate) {
-      const isSame = await bcrypt.compare(password, candidate.password);
-      if (isSame) {
-        req.session.user = candidate;
-        req.session.isAuthenticated = true;
-        req.session.save(err => {
-          if (err) throw err;
-          req.flash('loginError', 'Something went wrong');      
-          res.redirect('/subjects');
-        });
-      } else {
-        req.flash('loginError', 'Incorrect password');
-        res.redirect('/auth/login#login');
-      }
-    } else {
-      req.flash('loginError', 'No such user');
-      res.redirect('/auth/login#login');
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      req.flash('loginError', errors.array()[0].msg);
+      return res.status(422).redirect('/auth/login#login');
     }
+
+    const user = await User.findOne({ email: req.body.email });
+    req.session.user = user;
+    req.session.isAuthenticated = true;
+    req.session.save(err => {
+      if (err) throw err;
+      req.flash('loginError', 'Something went wrong');      
+      res.redirect('/subjects');
+    });
   } catch (err) {
     console.log(err);
   }
--- utils/validations.js
+++ utils/validations.js
@@ -2,6 +2,7 @@
 
 const { body } = require('express-validator');
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 exports.registerValidators = [
   body('email', 'Write correct email').isEmail().custom(async (value, {req}) => {
@@ -18,4 +19,22 @@ exports.registerValidators = [
     if (value !== req.body.password) throw new Error('Password must be same');
     return true;
   }).trim()
+];
+
+exports.loginValidators = [
+  body('email', 'No such email').isEmail().custom(async (value, {req}) => {
+    try {
+      const user = await User.findOne({ email: value });
+      if (!user) return Promise.reject('No such email');
+      return true;
+    } catch (err) {
+      console.log(err);
+    }
+  }),
+  body('password').custom(async (value, {req}) => {
+    const user = await User.findOne({ email: req.body.email });
+    const isSame = await bcrypt.compare(value, user.password);
+    if (!isSame) return Promise.reject('Incorrect password');
+    return true;
+  })
 ];
